0554cc7bceeda0227a487de7908a711bf85d021f
Abhinav <125545001+abhinav7677@users.noreply.github.com>
2023-11-07 17:16:21 +0530

    Time: 55 ms (23.96%), Space: 56.5 MB (20.83%) - LeetHub

---
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int eliminateMaximum(int[] dist, int[] speed) {
+       int wpTime = 0, killCount = 0, sz = dist.length;
+        PriorityQueue<Integer> reachTimes = new PriorityQueue<>();
+        for(int indx = 0; indx < sz; indx++){
+           
+            int timeToReach = (dist[indx] + speed[indx] - 1) / speed[indx];
+            reachTimes.offer(timeToReach);
+        }
+        while(!reachTimes.isEmpty()){
+            int currRTime = reachTimes.poll ();
+            if(wpTime >= currRTime && killCount > 0){
+                return killCount;
+            }else{
+                killCount++;
+            }
+            wpTime++;
+        }
+        return killCount; 
+    }
+}
